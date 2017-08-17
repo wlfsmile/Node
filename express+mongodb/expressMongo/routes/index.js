3ee45f904706a67a6e7c3042cb0e2628c18832b6
wlfsmile <wlffsmile@gmail.com>
2017-08-17 17:17:15 +0800

    express+mongodb简单实例

--- express+mongodb/expressMongo/routes/index.js
+++ express+mongodb/expressMongo/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+//mongodb
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var conn = mongoose.connect('mongodb://127.0.0.1:27017/express');
+var User = new mongoose.Schema({
+	name: String,
+	email: String,
+	age: String
+});
+
+var myModel = conn.model('user',User);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  myModel.findOne({name:"wlfsmile"},function(err,user){
+  	console.log(user);
+  	res.render('index',{title:'Express',user:user});
+  });
 });
 
 module.exports = router;
